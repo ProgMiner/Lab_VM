98fdd716828ccf8895a61422d44a45492c983d45
Eridan Domoratskiy <eridan200@mail.ru>
2024-04-03 13:17:02 +0300

    Implement interop library

--- src/main/java/ru/byprogminer/lamagraalvm/runtime/LamaFun.java
+++ src/main/java/ru/byprogminer/lamagraalvm/runtime/LamaFun.java
@@ -1,6 +1,87 @@
 package ru.byprogminer.lamagraalvm.runtime;
 
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.RootCallTarget;
+import com.oracle.truffle.api.dsl.Fallback;
+import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.frame.MaterializedFrame;
+import com.oracle.truffle.api.interop.InteropLibrary;
+import com.oracle.truffle.api.interop.TruffleObject;
+import com.oracle.truffle.api.library.ExportLibrary;
+import com.oracle.truffle.api.library.ExportMessage;
+import com.oracle.truffle.api.source.SourceSection;
+import com.oracle.truffle.api.utilities.TriState;
+import ru.byprogminer.lamagraalvm.LamaLanguage;
+import ru.byprogminer.lamagraalvm.nodes.builtin.StringBuiltin;
 
-public record LamaFun(RootCallTarget callTarget, MaterializedFrame parentScope) {}
+@ExportLibrary(InteropLibrary.class)
+public record LamaFun(RootCallTarget callTarget, MaterializedFrame parentScope) implements TruffleObject {
+
+    @ExportMessage
+    public boolean isExecutable() {
+        return true;
+    }
+
+    @ExportMessage
+    public Object execute(Object... arguments) {
+        return callTarget.call(arguments);
+    }
+
+    @ExportMessage
+    public boolean hasExecutableName() {
+        return callTarget.getRootNode().getName() != null;
+    }
+
+    @ExportMessage
+    public String getExecutableName() {
+        return callTarget.getRootNode().getName();
+    }
+
+    @ExportMessage
+    public boolean hasSourceLocation() {
+        return callTarget.getRootNode().getSourceSection() != null;
+    }
+
+    @ExportMessage
+    public SourceSection getSourceLocation() {
+        return callTarget.getRootNode().getSourceSection();
+    }
+
+    @ExportMessage
+    public boolean hasLanguage() {
+        return true;
+    }
+
+    @ExportMessage
+    public Class<LamaLanguage> getLanguage() {
+        return LamaLanguage.class;
+    }
+
+    @ExportMessage
+    public String toDisplayString(@SuppressWarnings("unused") boolean allowSideEffects) {
+        final StringBuilder sb = new StringBuilder();
+
+        StringBuiltin.string(this, sb);
+        return sb.toString();
+    }
+
+    @ExportMessage
+    @TruffleBoundary
+    public int identityHashCode() {
+        return System.identityHashCode(this);
+    }
+
+    @ExportMessage
+    static final class IsIdenticalOrUndefined {
+
+        @Specialization
+        static TriState isIdentical(LamaFun a, LamaFun b) {
+            return TriState.valueOf(a == b);
+        }
+
+        @Fallback
+        static TriState isIdentical(LamaFun a, Object b) {
+            return TriState.UNDEFINED;
+        }
+    }
+}
--- src/main/java/ru/byprogminer/lamagraalvm/runtime/LamaString.java
+++ src/main/java/ru/byprogminer/lamagraalvm/runtime/LamaString.java
@@ -1,10 +1,25 @@
 package ru.byprogminer.lamagraalvm.runtime;
 
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import com.oracle.truffle.api.dsl.Fallback;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.interop.InteropLibrary;
+import com.oracle.truffle.api.interop.InvalidArrayIndexException;
+import com.oracle.truffle.api.interop.TruffleObject;
+import com.oracle.truffle.api.interop.UnsupportedTypeException;
+import com.oracle.truffle.api.library.ExportLibrary;
+import com.oracle.truffle.api.library.ExportMessage;
+import com.oracle.truffle.api.strings.TruffleString;
+import com.oracle.truffle.api.utilities.TriState;
+import ru.byprogminer.lamagraalvm.LamaLanguage;
+import ru.byprogminer.lamagraalvm.nodes.builtin.StringBuiltin;
+
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 
+@ExportLibrary(InteropLibrary.class)
 @SuppressWarnings("MethodDoesntCallSuperMethod")
-public record LamaString(byte[] value) implements Cloneable {
+public record LamaString(byte[] value) implements TruffleObject, Cloneable {
 
     public static final Charset CHARSET = StandardCharsets.UTF_8;
 
@@ -20,5 +35,127 @@ public record LamaString(byte[] value) implements Cloneable {
 
     public static LamaString ofString(String str) {
         return new LamaString(str.getBytes(CHARSET));
+    }
+
+    @ExportMessage
+    public boolean isString() {
+        return true;
+    }
+
+    @ExportMessage
+    public String asString() {
+        return toString();
+    }
+
+    @ExportMessage
+    public TruffleString asTruffleString() {
+        return TruffleString.fromByteArrayUncached(value, TruffleString.Encoding.UTF_8);
+    }
+
+    @ExportMessage
+    public boolean hasArrayElements() {
+        return true;
+    }
+
+    @ExportMessage
+    public long getArraySize() {
+        return value.length;
+    }
+
+    @ExportMessage
+    public boolean isArrayElementReadable(long index) {
+        return 0 <= index && index < value.length;
+    }
+
+    @ExportMessage
+    public boolean isArrayElementModifiable(long index) {
+        return isArrayElementReadable(index);
+    }
+
+    @ExportMessage
+    public boolean isArrayElementInsertable(@SuppressWarnings("unused") long index) {
+        return false;
+    }
+
+    @ExportMessage
+    public boolean hasLanguage() {
+        return true;
+    }
+
+    @ExportMessage
+    public Class<LamaLanguage> getLanguage() {
+        return LamaLanguage.class;
+    }
+
+    @ExportMessage
+    public String toDisplayString(@SuppressWarnings("unused") boolean allowSideEffects) {
+        final StringBuilder sb = new StringBuilder();
+
+        StringBuiltin.string(this, sb);
+        return sb.toString();
+    }
+
+    @ExportMessage
+    @TruffleBoundary
+    public int identityHashCode() {
+        return System.identityHashCode(this);
+    }
+
+    @ExportMessage
+    static final class ReadArrayElement {
+
+        @Specialization(rewriteOn = {IndexOutOfBoundsException.class, ArithmeticException.class})
+        static byte readArrayElementInBounds(LamaString value, long index) {
+            return value.value[Math.toIntExact(index)];
+        }
+
+        @Specialization(replaces = "readArrayElementInBounds")
+        static Object readArrayElement(
+                LamaString value,
+                long index
+        ) throws InvalidArrayIndexException {
+            throw InvalidArrayIndexException.create(index);
+        }
+    }
+
+    @ExportMessage
+    static final class WriteArrayElement {
+
+        @Specialization(rewriteOn = {IndexOutOfBoundsException.class, ArithmeticException.class})
+        static void writeArrayElementInBounds(LamaString values, long index, byte value) {
+            values.value[Math.toIntExact(index)] = value;
+        }
+
+        @Specialization(replaces = "writeArrayElementInBounds")
+        static void writeArrayElement(
+                @SuppressWarnings("unused") LamaString values,
+                long index,
+                @SuppressWarnings("unused") byte value
+        ) throws InvalidArrayIndexException {
+            throw InvalidArrayIndexException.create(index);
+        }
+
+        @Fallback
+        static void writeArrayElement(
+                @SuppressWarnings("unused") LamaString values,
+                long index,
+                @SuppressWarnings("unused") Object value
+        ) throws UnsupportedTypeException {
+            throw UnsupportedTypeException.create(new Object[]{ value });
+        }
+    }
+
+    @ExportMessage
+    static final class IsIdenticalOrUndefined {
+
+        @Specialization
+        static TriState isIdentical(LamaString a, LamaString b) {
+            return TriState.valueOf(a == b);
+        }
+
+        @Fallback
+        static TriState isIdentical(LamaString a, Object b) {
+            return TriState.UNDEFINED;
+        }
     }
 }
